9cd5a1866ee182d1a2d3f181ad9e64b985b9c5e1
Demo <183799815@qq.com>
2024-04-30 19:14:34 +0800

    '#'

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,8 @@
 #include <thread>
 #include <game.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    PSTR lpCmdLine, int nCmdShow)
-{
-    auto game = new Game();
-    
-    initgraph(game->width, game->height);
-    BeginBatchDraw();
-    
-    std::thread th1([game](){game->controller();});
-    std::thread th2([game](){game->draw();});
-    th1.detach();
-    th2.detach();
-
-    while (true)
-    {
-        while (game->is_running) {
-            game->run();
-            Sleep(1000 / game->frame);
-        }
-    }
-    return 0;
-}
-
-// int main(int argc, char* args[])
+// int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+//     PSTR lpCmdLine, int nCmdShow)
 // {
 //     auto game = new Game();
     
@@ -45,3 +23,29 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 //     }
 //     return 0;
 // }
+
+void run() {
+    auto game = new Game();
+    
+    initgraph(game->width, game->height);
+    BeginBatchDraw();
+    
+    std::thread th1([game](){game->controller();});
+    std::thread th2([game](){game->draw();});
+    th1.detach();
+    th2.detach();
+
+    while (true)
+    {
+        while (game->is_running) {
+            game->run();
+            Sleep(1000 / game->frame);
+        }
+    }
+}
+
+int main(int argc, char* args[])
+{
+    run();
+    return 0;
+}
