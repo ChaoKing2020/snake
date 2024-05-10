f80e12b226be8f52fabd50df2b25b8a67d8c6117
Demo <183799815@qq.com>
2024-05-10 09:57:06 +0800

    '#'

--- test/main.cpp
+++ test/main.cpp
@@ -5,8 +5,8 @@ int main()
 {
     std::map<std::string, std::pair<std::string, std::string>> mp;
     mp["a"] = {"11", "22"};
-    mp.insert({"b", std::pair<std::string, std::string>("33", "44")});
-    for(auto& item : mp) {
+    mp.insert({"b", {"33", "44"}});
+    for(const auto& item : mp) {
         std::cout << item.first << " " << item.second.first << std::endl;
     }
     return 0;
